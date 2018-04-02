14424d1b44358bcfe64b0d8d6410e7562f492fb1
John Karvonen <jpkarvonen@gmail.com>
2018-04-02 22:10:05 +0000

    in progress

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,13 +5,11 @@ class Kele
     base_uri 'https://www.bloc.io/api/v1'
     
     def initialize(e, p)
-        post_response = self.class.post( '/sessions', body: {
+        response = self.class.post('/sessions', body: {
             email: e,
             password: p
         })
-        
-        @authorization_token = post_response['auth_token']
-        
+        @authorization_token = response["auth_token"]
         raise "Invalid Email or Password." if @authorization_token.nil?
     end
 end
