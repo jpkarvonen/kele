3ac3a45a64e9e86eb8cae8e649fe36574ff81282
John Karvonen <jpkarvonen@gmail.com>
2018-04-03 00:07:48 +0000

    user story one complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -2,15 +2,14 @@ require 'httparty'
 
 class Kele
     include HTTParty
-    debug_output $stdout
+    
+    base_uri 'https://www.bloc.io/api/v1'
     
     def initialize(e, p)
-        response = self.class.post('https://www.bloc.io/api/v1/sessions', body: {
+        response = self.class.post('/sessions', body: {
             email: e,
             password: p
         })
-        
-        print response
         
         @authorization_token = response["auth_token"]
         raise "Invalid Email or Password." if @authorization_token.nil?
